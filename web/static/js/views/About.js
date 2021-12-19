660dfe0af78e3d20cb77d5d0be606b24101cc044
poyang31 <poyang31@yahoo.com.tw>
2021-12-19 15:26:17 +0800

    Add LICENSE

--- web/static/js/views/About.js
+++ web/static/js/views/About.js
@@ -89,8 +89,7 @@ export default {
       <div class="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
         <dt class="text-sm font-medium text-gray-500">Copyright</dt>
         <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
-          Copyright &copy; 2021 Po-Yang Chen(https://github.com/poyang31) all
-          rights reserved.
+          Copyright &copy; 2021 Po-Yang Chen(https://github.com/poyang31)
         </dd>
       </div>
     </dl>
