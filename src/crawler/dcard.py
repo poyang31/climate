5bd1502c37485d8bb980dca01cc7da987bcad042
PoYang Chen <poyang31@yahoo.com.tw>
2021-12-20 01:51:02 +0000

    Reformat

--- src/crawler/dcard.py
+++ src/crawler/dcard.py
@@ -13,62 +13,59 @@ class Dcard(Spider):
     allowed_domains = ['dcard.tw']
     start_urls = ["https://dcard.tw"]
 
-def __init__(self, config: Config):
-    super().__init__(config)
-    self.DOWNLOADER_MIDDLEWARES.update({
-    'src.crawler.middlewares.ptt_cookies.CookiesMiddleware': 700
-    })
+    def __init__(self, config: Config):
+        super().__init__(config)
+        self.DOWNLOADER_MIDDLEWARES.update({
+            'src.crawler.middlewares.ptt_cookies.CookiesMiddleware': 700
+        })
 
-def capture(self, response: HtmlResponse) -> None:
-# Get Full Title
-    query = response.css("#__next > div.bvk29r-0.eFPEdc > div.bvk29r-2.etVvYS > div > div > div > div > article > div.sc-1eorkjw-1.ccTaOU > div > h1")
-    full_title = self.clear_html_tags_from_selectors(query)
-    tag = "NONE"
-    title = full_title
+    def capture(self, response: HtmlResponse) -> None:
+        # Get Full Title
+        query = response.css("#__next > div.bvk29r-0.eFPEdc > div.bvk29r-2.etVvYS > div > div > div > div > article > div.sc-1eorkjw-1.ccTaOU > div > h1")
+        full_title = self.clear_html_tags_from_selectors(query)
+        tag = "NONE"
+        title = full_title
+        # Get Class
+        query = response.css(
+            "#__next > div.bvk29r-0.eFPEdc > div.bvk29r-2.etVvYS > div > div > div > div > article > div.sc-1eorkjw-3.jJbLey > div:nth-child(1) > a")
+        class_ = self.clear_html_tags_from_selectors(query)
+        # Get Content
+        query = response.css(
+            '#__next > div.bvk29r-0.eFPEdc > div.bvk29r-2.etVvYS > div > div > div > div > article > div.sc-1eorkjw-5.hKSyLQ > div')
+        content = self.clear_html_tags_from_selectors(query)
+        # Get URL
+        url = response.url
+        # Get Words
+        words = self.explode_as_list(content)
 
-
-# Get Class
-    query = response.css("#__next > div.bvk29r-0.eFPEdc > div.bvk29r-2.etVvYS > div > div > div > div > article > div.sc-1eorkjw-3.jJbLey > div:nth-child(1) > a")
-    class_ = self.clear_html_tags_from_selectors(query)
-# Get Content
-    query = response.css('#__next > div.bvk29r-0.eFPEdc > div.bvk29r-2.etVvYS > div > div > div > div > article > div.sc-1eorkjw-5.hKSyLQ > div')
-    content = self.clear_html_tags_from_selectors(query)
-# Get URL
-    url = response.url
-# Get Words
-    words = self.explode_as_list(content)
-
-# Get Times
-    query = response.xpath('/html/body/div[1]/div[2]/div[2]/div/div/div/div/article/div[2]/div[2]/text()')
-    time_ = self.clear_html_tags_from_selectors(query)
-    if len(time_) == 17 :
-        struct_time = time.strptime(time_, "%Y年%m月%d日 %H:%M") # 轉成時間元組
-        time_stamp = int(time.mktime(struct_time)) # 轉成時間戳
-        created_time = time_stamp
-
-    elif len(time_) ==12 :
-        currentDateTime = datetime.datetime.now() #抓現在時間
-        date = currentDateTime.date()
-        year = date.strftime("%Y") #抓出年
-        time_=year+"年"+time_ #年分+時間合併
-        struct_time = time.strptime(time_, "%Y年%m月%d日 %H:%M") # 轉成時間元組
-        time_stamp = int(time.mktime(struct_time)) # 轉成時間戳
-        created_time = time_stamp
-    else:
-        created_time = 0
-
-
-
-    updated_time = 0
-# Storage
-    self.storage_article(Article(
-    origin=self.name,
-    class_=class_,
-    tag=tag,
-    title=title,
-    content=content,
-    url=url,
-    words=words,
-    created_time=created_time,
-    updated_time=updated_time,
-    ))
+        # Get Times
+        query = response.xpath(
+            '/html/body/div[1]/div[2]/div[2]/div/div/div/div/article/div[2]/div[2]/text()')
+        time_ = self.clear_html_tags_from_selectors(query)
+        if len(time_) == 17:
+            struct_time = time.strptime(time_, "%Y年%m月%d日 %H:%M")  # 轉成時間元組
+            time_stamp = int(time.mktime(struct_time))  # 轉成時間戳
+            created_time = time_stamp
+        elif len(time_) == 12:
+            currentDateTime = datetime.datetime.now()  # 抓現在時間
+            date = currentDateTime.date()
+            year = date.strftime("%Y")  # 抓出年
+            time_ = year+"年"+time_  # 年分+時間合併
+            struct_time = time.strptime(time_, "%Y年%m月%d日 %H:%M")  # 轉成時間元組
+            time_stamp = int(time.mktime(struct_time))  # 轉成時間戳
+            created_time = time_stamp
+        else:
+            created_time = 0
+            updated_time = 0
+        # Storage
+        self.storage_article(Article(
+            origin=self.name,
+            class_=class_,
+            tag=tag,
+            title=title,
+            content=content,
+            url=url,
+            words=words,
+            created_time=created_time,
+            updated_time=updated_time,
+        ))
--- src/crawler/gamer.py
+++ src/crawler/gamer.py
@@ -5,55 +5,56 @@ from .models import Article
 from .spider import Spider
 from ..kernel import Config
 
+
 class Gamer(Spider):
     name = "Gamer"
     allowed_domains = ['forum.gamer.com.tw']
     start_urls = ["https://forum.gamer.com.tw"]
 
-def __init__(self, config: Config):
-    super().__init__(config)
-    self.DOWNLOADER_MIDDLEWARES.update({
-    'src.crawler.middlewares.ptt_cookies.CookiesMiddleware': 700
-    })
+    def __init__(self, config: Config):
+        super().__init__(config)
+        self.DOWNLOADER_MIDDLEWARES.update({
+            'src.crawler.middlewares.ptt_cookies.CookiesMiddleware': 700
+        })
 
-def capture(self, response: HtmlResponse) -> None:
-    query = response.xpath("/html/body/div[5]/div/div[2]/section[2]/div[2]/div[1]/h1")
-    full_title = self.clear_html_tags_from_selectors(query)
-    if full_title.strip() != "":
-        title_data = fullmatch(r"\【(.*?)\】(.*?)", full_title)
-        # Get Tag and Title
-        if title_data is None:
-            tag = "Unknown"
-            title = full_title
-        else:
-            tag = title_data.group(1)
-            title = title_data.group(2)
-# Get Class
-    query = response.xpath('/html/body/div[3]/ul/li[1]/a') #抓不到版名 只顯示[]
-    class_ = self.clear_html_tags_from_selectors(query)
-# Get Content
-    query = response.xpath('/html/body/div[5]/div/div[2]/section[2]/div[2]/div[2]/article/div')
-    content = self.clear_html_tags_from_selectors(query)
-# Get URLSs
-    url = response.url
-# Get Words
-    words = self.explode_as_list(content)
-# Get Times
-    query = response.xpath('/html/body/div[5]/div/div[2]/section[1]/div[2]/div[1]/div[3]/a[1]/@data-mtime') #抓不到時間 只顯示[]
-
-
-
-
-# Storage
-    self.storage_article(Article(
-    origin=self.name,
-    class_=class_,
-    tag=tag,
-    title=title,
-    content=content,
-    url=url,
-    words=words,
-    created_time=0,
-    updated_time=0
-
-    ))
+    def capture(self, response: HtmlResponse) -> None:
+        query = response.xpath(
+            "/html/body/div[5]/div/div[2]/section[2]/div[2]/div[1]/h1")
+        full_title = self.clear_html_tags_from_selectors(query)
+        if full_title.strip() != "":
+            title_data = fullmatch(r"\【(.*?)\】(.*?)", full_title)
+            # Get Tag and Title
+            if title_data is None:
+                tag = "Unknown"
+                title = full_title
+            else:
+                tag = title_data.group(1)
+                title = title_data.group(2)
+        # Get Class
+        # ToDo: 抓不到版名 只顯示[]
+        query = response.xpath('/html/body/div[3]/ul/li[1]/a')
+        class_ = self.clear_html_tags_from_selectors(query)
+        # Get Content
+        query = response.xpath(
+            '/html/body/div[5]/div/div[2]/section[2]/div[2]/div[2]/article/div')
+        content = self.clear_html_tags_from_selectors(query)
+        # Get URLSs
+        url = response.url
+        # Get Words
+        words = self.explode_as_list(content)
+        # Get Times
+        # ToDo: 抓不到時間 只顯示 []
+        query = response.xpath(
+            '/html/body/div[5]/div/div[2]/section[1]/div[2]/div[1]/div[3]/a[1]/@data-mtime')
+        # Storage
+        self.storage_article(Article(
+            origin=self.name,
+            class_=class_,
+            tag=tag,
+            title=title,
+            content=content,
+            url=url,
+            words=words,
+            created_time=0,
+            updated_time=0
+        ))
