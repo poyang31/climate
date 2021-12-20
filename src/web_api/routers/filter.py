db27f06ae2f6aa5121d11356f7162411cfa44ae0
poyang31 <poyang31@yahoo.com.tw>
2021-12-21 04:08:23 +0800

    Update

--- src/web_api/routers/filter.py
+++ src/web_api/routers/filter.py
@@ -1,5 +1,8 @@
-from fastapi import APIRouter
 from typing import Union
+
+from fastapi import APIRouter
+from pymongo import DESCENDING
+
 from . import articles_collection
 from ...crawler.models import Article
 
@@ -7,8 +10,14 @@ router = APIRouter()
 
 
 @router.get("/")
-def read_filter(keyword: str = "") -> Union[list, None]:
-    if not keyword:
+def read_filter(keyword: str = "", page: int = 1) -> Union[list, None]:
+    if page < 0 or not keyword:
         return None
-    result = articles_collection.find({"words": {"$in": [keyword]}})
+    offset = (page - 1) * 10
+    limit = page * 10
+    result = articles_collection \
+        .find({"words": {"$in": [keyword]}}) \
+        .sort("updated_time", DESCENDING) \
+        .skip(offset) \
+        .limit(limit)
     return [Article.parse_obj(i) for i in result]
